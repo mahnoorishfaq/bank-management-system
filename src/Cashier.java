f65018896ccdfd61523ab866ba1cef3f7e5a0621
Mahnoor Ishfaq <164208590+mahnoorishfaq@users.noreply.github.com>
2026-08-24 23:09:23 +0500

    Update Cashier class

--- src/Cashier.java
+++ src/Cashier.java
@@ -7,24 +7,21 @@ class Cashier extends Person {
     private boolean isAvailable;
     private ArrayList<Cashier> cashierList;
 
-    // Constructor to initialize Cashier details
     public Cashier(String cashierName, String cashierContact, String cashierId)
             throws InvalidNameException, InvalidContactNumberException, InvalidAddressException {
         super(cashierName, "Bank counter", cashierContact);
         this.cashierId = cashierId;
-        this.isAvailable = true; // Initially, cashier is available
+        this.isAvailable = true; 
     }
 
     public String getCashierId() {
 
         return cashierId;
     }
-    // Setter for cashierList
     public void setCashierList(ArrayList<Cashier> cashierList) {
         this.cashierList = cashierList;
     }
 
-    // Getter for cashierList
     public ArrayList<Cashier> getCashierList() {
         return cashierList;
     }
@@ -36,7 +33,7 @@ class Cashier extends Person {
 
     public void dealWithCustomer(Customer customer) {
         if (isAvailable) {
-            this.isAvailable = false;  // Cashier becomes unavailable once dealing with customer
+            this.isAvailable = false;  
             System.out.println("Cashier " + super.name + " is serving customer " + customer.getName());
         } else {
             System.out.println("Cashier " + super.name + " is not available.");
@@ -57,16 +54,13 @@ class Cashier extends Person {
         return super.name;
     }
 
-        // Example method to approve a loan
         public void approveLoan(Loan loan) {
-            // Logic to approve loan, for example:
             loan.approveLoan();
             System.out.println("Loan approved by Cashier: " + getName());
             System.out.println("Loan Amount: " + loan.getLoanAmount());
             System.out.println("EMI: " + loan.calculateEMI());
         }
 
-        // Other cashier-related methods like checking availability
 
         public String getContact() {
             return contact;
