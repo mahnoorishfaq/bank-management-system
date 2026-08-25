4bbb1fc1a3b782bb615412194a3171adb9dee116
Mahnoor Ishfaq <164208590+mahnoorishfaq@users.noreply.github.com>
2026-08-25 22:06:10 +0500

    Edit Transaction class
    Removed comments for constructor and processTransaction method.

--- src/Transaction.java
+++ src/Transaction.java
@@ -6,14 +6,12 @@ class Transaction {
     private double amount;
     private int timestamp;
 
-    // Constructor to initialize transaction details
     public Transaction(String transactionId, String type, double amount, int timestamp){
         this.transactionId = transactionId;
         this.type = type;
         this.amount = amount;
         this.timestamp = timestamp;
     }
-    //Method to process the transaction
     public void processTransaction(){
         System.out.println("Transaction ID: " + transactionId + ", Type: " + type + ", Amount: " + amount + ", Timestamp: " + timestamp);
     }
