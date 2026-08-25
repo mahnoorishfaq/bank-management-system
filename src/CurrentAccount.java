c0b5dc41823eb3317e68e3116ff14f70f3f9e98a
Mahnoor Ishfaq <164208590+mahnoorishfaq@users.noreply.github.com>
2026-08-25 21:59:41 +0500

    Edit CurrentAccount methods for clarity

--- src/CurrentAccount.java
+++ src/CurrentAccount.java
@@ -1,24 +1,21 @@
 public class CurrentAccount extends Account {
 
-    // Constructor to initialize CurrentAccount
     public CurrentAccount(double balance) {
-        super(balance);  // Call superclass constructor to initialize balance
+        super(balance);  
     }
 
-    // Method to deposit money into current account
     @Override
     public void deposit(double amount) {
-        balance += amount;  // Add the amount to the current balance
+        balance += amount;  
         System.out.println("Deposited " + amount + " to current account. New balance: " + balance);
     }
 
-    // Method to withdraw money from current account
     @Override
     public void withdraw(double amount) throws Exception {
         if (amount > balance) {
             throw new Exception("Insufficient funds in current account.");
         }
-        balance -= amount;  // Deduct the amount from balance
+        balance -= amount;  
         System.out.println("Withdrew " + amount + " from current account. New balance: " + balance);
     }
 
